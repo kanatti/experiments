dcce2cdc3c7835bf989e40d4934d64a0b4268dee
Balagopal Kanattil <balukanattil@gmail.com>
2023-11-24 22:36:30 -0500

    [networking] More on showip

---
+++ READING.md
@@ -0,0 +1,9 @@
+# Interesting Projects
+
+Just a bunch of projects or crates for some source reading.
+
+1. [itertools](https://github.com/rust-itertools/itertools/tree/master)
+2. [ratatui](https://github.com/ratatui-org/ratatui)
+3. [ox](https://github.com/curlpipe/ox)
+4. [core-foundation](https://github.com/servo/core-foundation-rs)
+5. [alacritty](https://github.com/alacritty/alacritty)
--- networking/showip.c
+++ networking/showip.c
@@ -10,31 +10,65 @@
 ❯ ./showip google.com
 IP Addresses for google.com:
 
-  IPV6: 2607:f8b0:400b:803::200e
+  IPV6: 2607:f8b0:400b:807::200e
   IPV4: 142.251.41.46
+❯ ./showip google.com  --ipv4
+IP Addresses for google.com:
+
+  IPV4: 142.251.41.46
+❯ ./showip google.com  --ipv6
+IP Addresses for google.com:
+
+  IPV6: 2607:f8b0:400b:807::200e
 */
+
+
+void printUsage() {
+    fprintf(stderr, "usage: \nshowip hostname\nshowip hostname --ipv4\nshowip hostname --ipv6\n");
+}
 
 
 // Prints IPV4 and IPV6 Addresses
 int main(int argc, char *argv[]) {
+    // Uninitialized, these can garbage values.
     struct addrinfo hints, *res, *p;
     int status;
     char ipstr[INET6_ADDRSTRLEN];
 
-    if (argc != 2) {
-        fprintf(stderr, "usage: showip hostname\n");
+    if (argc < 2 || argc > 3) {
+        printUsage();
+        return 1;
     }
 
+    char *hostname = argv[1];
+    int ai_family = AF_UNSPEC;
+
+    if (argc == 3) {
+        if (strcmp(argv[2], "--ipv4") == 0) {
+            ai_family = AF_INET;
+        } else if (strcmp(argv[2], "--ipv6") == 0) {
+            ai_family = AF_INET6;
+        } else {
+            printUsage();
+            return 1;
+        }
+    }
+
+    // First I thought memset involves some allocation.
+    // But thats not the case. hints can have garbage value in stack.
+    // Applies to both basic types and pointers (Garbage pointer values).
+    // Memset sets value of basic stack types to zero and pointers to null.
+    // No allocation, no interaction with heap.
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = ai_family;
     hints.ai_socktype = SOCK_STREAM;
 
-    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+    if ((status = getaddrinfo(hostname, NULL, &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         return 2;
     }
 
-    printf("IP Addresses for %s:\n\n", argv[1]);
+    printf("IP Addresses for %s:\n\n", hostname);
 
     for (p = res; p != NULL; p = p->ai_next) {
         void *addr;
